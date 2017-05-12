27dd133aeeb036bd4831aacaab50460e057db27c
Ke Deng <ke.deng.17@dartmouth.edu>
2017-05-12 16:18:35 -0400

    stripping down app, header bar

--- src/index.js
+++ src/index.js
@@ -5,16 +5,9 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import App from './components/app';
 
-
 import './style.scss';
 import reducers from './reducers';
 
-// import App from './components/app';
-
-// map posts array to keys
-
-
-// import ReactDOM from 'react-dom';
 // this creates the store with the reducers, and does some other stuff to initialize devtools
 const store = createStore(reducers, {}, compose(
   applyMiddleware(thunk),
