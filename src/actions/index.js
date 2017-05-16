94885cfa40e38671ea7b2146f2d7750b5a64e08d
dylansc <dylan.c.scandinaro.17@dartmouth.edu>
2017-05-15 20:51:26 -0400

    added post functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,24 @@
-// import axios from 'axios';
+import axios from 'axios';
 
 // keys for action types
- export const ActionTypes = {
-   ADD_MUSIC_NOTE: 'ADD_MUSIC_NOTE',
- };
+const ROOT_URL = 'https://prettymusicmaker.herokuapp.com';
+
+
+export const ActionTypes = {
+  ADD_MUSIC_TILE: 'ADD_MUSIC_TILE',
+};
 
  // dispatch action with a column id, and the clicked note array
- export function addTile(data) {
-   return (dispatch) => {
-     dispatch({ type: ActionTypes.ADD_MUSIC_NOTE });
-   };
- }
+export function addTile(data) {
+  return (dispatch) => {
+    const fields = { title: 'title', tags: 'tags', content: data, cover_url: 'cover' };
+    axios.post(`${ROOT_URL}/api/music`, fields).then((response) => {
+      // do something with response.data  (some json)
+      dispatch({ type: ActionTypes.ADD_MUSIC_TILE });
+    }).catch((error) => {
+      console.log('caught error in addTile:');
+      console.log(error);
+      // hit an error do something else!
+    });
+  };
+}
--- src/reducers/music-reducer.js
+++ src/reducers/music-reducer.js
@@ -2,7 +2,7 @@ import { ActionTypes } from '../actions';
 
 const MusicReducer = (state = 0, action) => {
   switch (action.type) {
-    case ActionTypes.ADD_MUSIC_NOTE:
+    case ActionTypes.ADD_MUSIC_TILE:
       return { test: 1 };
     default:
       return state;
