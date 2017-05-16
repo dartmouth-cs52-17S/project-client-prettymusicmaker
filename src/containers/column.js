5dbe512e1b02d9887a208b3f231e724c4a724155
dylansc <dylan.c.scandinaro.17@dartmouth.edu>
2017-05-15 23:05:47 -0400

    working on rendering multiple tiles

--- src/containers/column.js
+++ src/containers/column.js
@@ -1,5 +1,7 @@
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
+import update from 'react-addons-update'; // ES6
+
 import { addTile } from '../actions';
 
 
@@ -12,30 +14,60 @@ class Column extends Component {
     console.log('in constructor');
     super(props);
     this.state = {
-      notes: {},
+      tiles: {
+        1: false,
+        2: true,
+        3: false,
+      },
     };
     this.onTileClick = this.onTileClick.bind(this);
+    this.renderTiles = this.renderTiles.bind(this);
   }
 
-  onTileClick() {
-    this.setState({ notes: event.target.value });
+  onTileClick(event) {
+    // this.setState({ tiles: event.target.value });
     console.log('in addNoteClick props is');
     console.log(this.props);
-    console.log('e.t.v is :');
-    console.log(event.target.value);
+    console.log('e.t is :');
+    console.log(event.target);
+    // const newState = this.state.tiles;
+    // newState.[event.target.value]=true;
+    // const tilenum = event.target.title;
+    // const checked = this.state.tiles[event.target.title];
+    // update(this.state.tiles, { [event.target.title]: false });
+
+    this.setState({ tiles: update(this.state.tiles, { [event.target.title]: false }),
+    });
+
+
     // eslint-disable-next-line no-unused-vars
     // const noteToAdd = <Note id={100} note={noteInfo} />;
     // console.log('printing noteToAdd');
     // console.log(noteToAdd);
-    this.props.addTile({ notes: 1 });
+    // this.props.addTile({ tiles[] });
   }
+
+  renderTiles() {
+    // if (this.state.1) {
+    console.log('this.props.posts in renderposts');
+    console.log(this.state.tiles);
+    return this.state.tiles.map((tile, i) => {
+      return (
+        <input type="checkbox" title={i} className="tile" onChange={this.onTileClick} checked={tile} />
+      );
+    });
+    // } else {
+    //   return 'loading';
+    // }
+  }
+
 
   render() {
     return (
       <div id="inputwindow">
         <div id="inputdiv">
           <h1> Notes below here </h1>
-          <input type="checkbox" className="tile" onClick={this.onTileClick} />
+          {this.renderTiles()}
         </div>
       </div>
 
