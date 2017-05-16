19db772da8c36e2d4410fdaa520928517ea8f41a
dylansc <dylan.c.scandinaro.17@dartmouth.edu>
2017-05-16 17:37:35 -0400

    refactored col to music

---
+++ src/components/musicPortion.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import MusicPortionContainer from '../containers/musicPortion';
+
+// import Note from './note';
+
+// eslint-disable-next-line react/prefer-stateless-function
+const MusicPortion = (props) => {
+  return (
+    <div>
+      <h1> in MusicPortion </h1>
+      <MusicPortionContainer />
+    </div>
+  );
+};
+
+export default MusicPortion;
