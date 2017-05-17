31899c4871357c3eedbbed1b8933fe5dde5b8c04
Ke Deng <ke.deng.17@dartmouth.edu>
2017-05-17 16:52:18 -0400

    toggle nav signin/signout

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,14 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 import { NavLink } from 'react-router-dom';
-// <NavLink to="/"><input id="button_id" type="submit" value="Home" /></NavLink>
-const Nav = (props) => {
-  return (
-    <nav>
-      <NavLink exact to="/"><i className="fa fa-home" aria-hidden="true" /></NavLink>
-      <NavLink id="nav-button" exact to="/profile"><i className="fa fa-user" aria-hidden="true" /></NavLink>
-      <NavLink id="nav-button" exact to="/editor"><i className="fa fa-plus" aria-hidden="true" /></NavLink>
-    </nav>
-  );
-};
+import { signoutUser } from '../actions';
 
-export default Nav;
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.renderSignIn = this.renderSignIn.bind(this);
+  }
+
+  renderSignIn() {
+    if (this.props.authenticated) {
+      console.log('authenticated!!');
+      return (
+        <li><NavLink exact to="/" onClick={() => this.props.signoutUser(this.props.history)}>log Out</NavLink></li>
+      );
+    }
+    return (
+      <span />
+    );
+  }
+
+  // Icon version
+  render() {
+    return (
+      <nav>
+        <NavLink exact to="/"><i className="fa fa-home" aria-hidden="true" /></NavLink>
+        <NavLink id="nav-button" exact to="/profile"><i className="fa fa-user" aria-hidden="true" /></NavLink>
+        <NavLink id="nav-button" exact to="/editor"><i className="fa fa-plus" aria-hidden="true" /></NavLink>
+        {this.renderSignIn()}
+      </nav>
+    );
+  }
+}
+
+// Text-only version
+// render() {
+//   return (
+//     <nav>
+//       <NavLink exact to="/">home</NavLink>
+//       <NavLink id="nav-button" exact to="/profile">profile</NavLink>
+//       <NavLink id="nav-button" exact to="/editor">create</NavLink>
+//       {this.renderSignIn()}
+//     </nav>
+//   );
+// }
+
+const mapStateToProps = state => (
+  {
+    authenticated: state.auth.authenticated,
+  });
+
+export default withRouter(connect(mapStateToProps, { signoutUser })(Nav));
