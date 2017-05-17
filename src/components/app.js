2d775c570e60b83f285621976b51ffd52aff7802
Ke Deng <ke.deng.17@dartmouth.edu>
2017-05-17 15:44:25 -0400

    routing in app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import React from 'react';
 import FallBack from './fallback';
-// import Nav from './nav';
 import Splash from './splash';
 import MusicPortion from './musicPortion';
 import Profile from './profile';
@@ -14,6 +13,8 @@ const App = (props) => {
           <Route exact path="/" component={Splash} />
           <Route path="/editor" component={MusicPortion} />
           <Route path="/profile" component={Profile} />
+          <Route path="/signin" component={Splash} />
+          <Route path="/signup" component={Splash} />
           <Route component={FallBack} />
         </Switch>
       </div>
