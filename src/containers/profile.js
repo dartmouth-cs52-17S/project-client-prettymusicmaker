2320e414baa5abec7de35e5ce8706c2791e2ecf3
Ke Deng <ke.deng.17@dartmouth.edu>
2017-05-23 15:16:02 -0400

    changing profile styling

--- src/containers/profile.js
+++ src/containers/profile.js
@@ -38,7 +38,6 @@ class Profile extends Component {
     return (
       <div id="profilepage">
         <div id="profilebar">
-          <img alt="" src="https://img.clipartfest.com/d768243d1716e20f370362fcb75d346b_balloon-clipart-balloon-images-free-clip-art_615-1044.png" />
           <div id="profileinfo">
             <div id="profilename">username here</div>
             <div id="profiletag">some profile info here</div>
