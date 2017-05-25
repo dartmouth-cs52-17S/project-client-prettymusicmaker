6b1800a73fc6c03c752b453f39a7e4999369c9ec
Vanny Nguyen <van.p.nguyen.17@dartmouth.edu>
2017-05-25 18:23:57 -0400

    merge

--- src/containers/musicPortion.js
+++ src/containers/musicPortion.js
@@ -134,7 +134,7 @@ class MusicPortion extends Component {
 
   // reset the notes to false when cancel is clicked
   onCancelClick(e) {
-    this.setState({ modalIsOpen: false });
+    this.closeModal();
     this.clearTiles();
     this.stopPlaying();
   }
@@ -220,10 +220,10 @@ class MusicPortion extends Component {
     this.setState({ modalIsOpen: true });
   }
 
-  afterOpenModal() {
-  // references are now sync'd and can be accessed.
-    this.subtitle.style.color = '#f00';
-  }
+  // afterOpenModal() {
+  // // references are now sync'd and can be accessed.
+  //   // this.subtitle.style.color = '#f00';
+  // }
 
   closeModal() {
     this.setState({ modalIsOpen: false });
